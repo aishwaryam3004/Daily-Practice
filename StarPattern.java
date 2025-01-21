76dc8dd77ab85ac8278c0c71b4430fbe1f44f3ca
Aishwarya M <149880741+aishwaryam3004@users.noreply.github.com>
2025-01-21 22:07:32 +0530

    STarPattern.java

--- StarPattern.java
+++ StarPattern.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
-
 public class StarPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
         int r = sc.nextInt();
-        for (int i = 1; i <= r; i++) {
-            for (int j = 1; j <= r - i; j++) {
+        for (int i = 1; i <= r; i++) // outer loop {
+            for (int j = 1; j <= r - i; j++) // inner loop{
                 System.out.print(" ");
             }
             for (int k = 1; k <= (2 * i - 1); k++) {
