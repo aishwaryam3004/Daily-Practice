6b429a838211a92694a365811c7fcd7f738c34ec
Aishwarya M <149880741+aishwaryam3004@users.noreply.github.com>
2025-01-26 17:19:21 +0530

    Operator

---
+++ Arthimetic.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Arthimetic{
+	public static void main(String[] args){
+		int a = 12;
+		int b = 46;
+		System.out.println("Sum:" + (a+b));
+		System.out.println("difference:" + (a-b));
+		System.out.println("product:" + (a*b));
+		System.out.println("division:" + (a/b));
+		System.out.println("modulus:" + (a%b));
+	}
+}
---
+++ Relationaloperator.java
@@ -0,0 +1,10 @@
+public class Relationaloperator{
+	public static void main(String[] args){
+		int a = 40;
+   	    System.out.println("Greater than="+ (a > 10)); // Check 'a' is greater than 10 using greater than symbol
+		System.out.println("less than =" + (a<10));    // Check 'a' is less than 10 using less than symbol
+		System.out.println("equals:" + (a==10)); // Check 'a' is equals to 10 using greater than symbol
+
+	}
+}
+		
