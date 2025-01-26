38e10daed6de3eb35bd358e8c9eff786954f7b5d
Aishwarya M <149880741+aishwaryam3004@users.noreply.github.com>
2025-01-26 17:52:15 +0530

    rectangle.java

--- rectangle.java
+++ rectangle.java
@@ -3,10 +3,10 @@ public class rectangle{
   	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the height:");
-		float h = sc.nextFloat();
+		float h = sc.nextFloat(); 
 		System.out.print("Enter the width:");
 		float w = sc.nextFloat();
-		float area = h*w;
+		float area = h*w;  // using * operator multiply the two input to find area of the rectangle
 		System.out.println("Area=" + area);
 	}
 } 
