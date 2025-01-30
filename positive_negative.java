384e5348ecc6f45e08b9188c3a705a68c9e5ba3b
Aishwarya M <149880741+aishwaryam3004@users.noreply.github.com>
2025-01-31 05:27:37 +0530

    positive_negative

---
+++ positive_negative.java
@@ -0,0 +1,13 @@
+public class positive_negative{
+	public static void main(String[] args){
+	int n = 66;
+	if (n>0){ // if a number is greater than 0 it is a positive number
+	         System.out.println("Positive");
+	} else if(n==0){ // if a number is equals to zero it will print zero
+		 System.out.println("ZERO");
+	 } else{ // The above two condition is not satisfied it print Negative
+		 System.out.println("Negative");
+	    }
+	}
+}
+		
