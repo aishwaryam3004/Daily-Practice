f77fe2c917483049230fa4c8056852267a20e45d
Aishwarya M <149880741+aishwaryam3004@users.noreply.github.com>
2025-01-30 17:54:44 +0530

    infinte_while_loop

---
+++ whileinfinteloop.java
@@ -0,0 +1,25 @@
+/*1.Imagine you‟re on a quest to discover never-ending loops. Create a program that 
+demonstrates two types of endless journeys: one using a for loop and another using a 
+while loop. How will you set up these loops to keep running forever, showcasing their 
+infinite nature?*/
+
+class whileinfinteloop{
+	public static void main(String[] args){
+		while(true){
+			System.out.println("Infinte loop");
+		}
+	}
+}
+
+/* while loop
+int i=1,x = 1
+while(x == i){ -> This while loop is intalized with same value for both variable it always true
+	System.out.println("Infinte loop");
+	}
+}
+
+boolean loop = true;
+while(loop){ -> using boolean variable we can give the condition true  it taken true only for all the iteration
+	System.out.println("Infinte loop");
+}
+*/
