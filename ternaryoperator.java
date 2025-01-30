8a29e05e06ef1231a1fa9ab203870fdd7755c19f
Aishwarya M <149880741+aishwaryam3004@users.noreply.github.com>
2025-01-30 20:53:12 +0530

    ternaryoperator

---
+++ ternaryoperator.java
@@ -0,0 +1,7 @@
+public class ternaryoperator{
+	public static void main(String[] args){
+	int a = 78;
+	boolean b = (a<20) ?true :false;  // it return true the boolean expression is true otherwise false
+	System.out.println(b);
+	}
+}
