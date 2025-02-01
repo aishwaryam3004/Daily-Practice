41a65da0ada5e4837a63078e2c73af6e31a99443
Aishwarya M <149880741+aishwaryam3004@users.noreply.github.com>
2025-02-01 23:37:25 +0530

    Inverted_Right_Angle_Triangle

---
+++ Invertedrighttriangle.java
@@ -0,0 +1,13 @@
+// INVERTED RIGHT ANGLE TRIANGLE
+
+class Invertedrighttriangle{
+	public static void main(String[] args){
+	    for(int i=1;i<=5;i++){ // OUTER LOOP RUNS FROM 1 TO 5
+	        for(int j = i;j<=5;j++){ // INNER LOOP j IS INITALIZE i : if i = 1 , j= 1 and checks 1 <=5 for first it print 5 start for outer 2nd iteration i is 2 now j = 2 ,2<=5 it print from 2,3,4,5 only 4 star it will continue
+	            System.out.print("* ");
+	            
+	        }
+	        System.out.println();
+					}
+				}
+		}
