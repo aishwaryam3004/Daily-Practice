f6a5a402ae5f284c97bc132c11a7e3dd3b6d54df
Aishwarya M <149880741+aishwaryam3004@users.noreply.github.com>
2025-02-05 21:53:50 +0530

    mettl_ISEVEN_PRATICE_1

---
+++ ISEVEN.java
@@ -0,0 +1,18 @@
+//ISEVEN 
+
+import java.io.*; 
+import java.util.*;  
+
+// Read only region start 
+class UserMainCode { 
+    public int isEven(int input1) { 
+        // Read only region end 
+
+        // Check if the number is even
+        if (input1 % 2 == 0) {  
+            return 2;  
+        } else {  
+            return 1;  
+        }  
+    }  
+}
